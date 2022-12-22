a76b36e89729a10fb1261c9a74bd3ee194ad02a3
donatomontrone <donato.montrone@outlook.com>
2022-12-22 11:39:04 +0100

    fix typing errors in snack-n

---
+++ snack-3/js/script.js
@@ -0,0 +1,21 @@
+/***
+Chiedere all'API 10 nomi da inserire in un array di invitati.
+Una volta generata la lista chiedere all'utente di dire il proprio nome.
+Se è uno degli invitati ritornare un messaggio di benvenuto, altrimenti di accesso negato.
+*/
+
+// https://flynn.boolean.careers/exercises/api/random/name (ritorna un nome)
+
+const { createApp } = Vue
+
+    createApp({
+        data() {
+            return {
+            }
+        },
+        methods : {
+
+        },
+        mounted(){
+        }
+}).mount('#app')
